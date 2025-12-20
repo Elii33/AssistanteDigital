2d2d72d5f16d32f1e56817ce239b25fc45f87582
Arnaud <derisbourgarnaud@gmail.com>
2025-12-20 20:53:54 +0100

    add pk emailJs

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -5,10 +5,10 @@ import emailjs from '@emailjs/browser';
   providedIn: 'root'
 })
 export class EmailService {
-  // Clés EmailJS - À CONFIGURER après création du compte
-  private serviceId = 'mon_email_service_ldng';
-  private templateId = 'template_c5a9snv';
-  private publicKey = 'ivNsCLAyvZHZ38jSb';
+  // Clés EmailJS - Configuré pour ElisAssist
+  private serviceId = 'service_ugrbmwr';
+  private templateId = 'template_3uz2spk';
+  private publicKey = 'pdsy07zUPUZgThsFh';
 
   constructor() {
     // Initialiser EmailJS avec votre clé publique
@@ -18,11 +18,10 @@ export class EmailService {
   async sendEmail(formData: any): Promise<any> {
     try {
       const templateParams = {
-        from_name: formData.name, 
-        from_email: formData.email,
-        project_type: formData.projectType,
-        message: formData.message,
-        to_email: 'derisbourgarnaud@gmail.com'
+        name: formData.name,
+        email: formData.email,
+        projectType: formData.projectType,
+        message: formData.message
       };
 
       const response = await emailjs.send(
